a77920e22661e87d0fa04902e00249442523ba93
Deep Shah <deepshah983@gmail.com>
2024-08-06 17:33:21 +0530

    Added token verification for category, client, stock

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -2,10 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const verifyToken = (req, res, next) => {
     const token = req.header('Authorization');
-
-    if (!token) {
-        return res.status(401).json({ msg: "No token, authorization denied" });
-    }
+    if (!token) return res.status(401).json({ msg: "Access denied. No token provided." });
 
     try {
         const decoded = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
--- src/routes/stock.routes.js
+++ src/routes/stock.routes.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import stockController from '../controllers/stock.controller.js'; // Adjust path if necessary
+import stockController from '../controllers/stock.controller.js';
+import verifyToken from '../middleware/auth.middleware.js';
 
 const stockRoute = express.Router();
 const { stockAdd, getAllStocks, getStockById, updateStock, deleteStock } = stockController;
 
-stockRoute.post('/api/add-stock', stockAdd);
-stockRoute.get('/api/stocks', getAllStocks);
-stockRoute.get('/api/stocks/:id', getStockById);
-stockRoute.put('/api/stocks/:id', updateStock);
-stockRoute.delete('/api/stocks/:id', deleteStock);
+stockRoute.post('/api/add-stock',verifyToken, stockAdd);
+stockRoute.get('/api/stocks',verifyToken, getAllStocks);
+stockRoute.get('/api/stocks/:id',verifyToken, getStockById);
+stockRoute.put('/api/stocks/:id',verifyToken, updateStock);
+stockRoute.delete('/api/stocks/:id',verifyToken, deleteStock);
 
-export default stockRoute; // Default export
+export default stockRoute;
