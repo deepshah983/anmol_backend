c2a75f99748c48e4498175268fdc18073be104b0
Deep Shah <deepshah983@gmail.com>
2024-08-06 14:59:42 +0530

    Added new routes, form data parsing, JWT_SECRET in index file.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import { connection } from './db/connection.js'; // Ensure this path is correct
 import 'dotenv/config';
-import clientRoute from './routes/client.route.js'; // Default import, adjust path if necessary
+import clientRoute from './routes/client.routes.js'; // Default import, adjust path if necessary
+import userRoute from './routes/user.routes.js';
+import categoryRoute from './routes/category.routes.js';
+import stockRoute from './routes/stock.routes.js';
+import authRoute from './routes/auth.routes.js';
+
 
 const app = express();
-app.use(express.json());
+
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3000;
 
@@ -14,7 +21,17 @@ app.get("/", (req, res) => {
     });
 });
 
+if (!process.env.JWT_SECRET) {
+    console.error('FATAL ERROR: JWT_SECRET is not defined.');
+    process.exit(1);
+}
+
+// Use routes
+app.use(userRoute);
 app.use(clientRoute);
+app.use(categoryRoute);
+app.use(stockRoute);
+app.use(authRoute);
 
 app.listen(port, () => {
     console.log(`Project run on this port ${port}`);
