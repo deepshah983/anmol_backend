e0e6970f4e8cbb4d04bc3497942db8ee550d34b5
Nikunj Ravajibhai Savaliya <BL0113@Bliss.com>
2024-10-09 15:49:32 +0530

    feat: add sorting in users

--- src/db/connection.js
+++ src/db/connection.js
@@ -4,10 +4,7 @@ import 'dotenv/config'; // Load environment variables
 const mongoURI = process.env.MONGO_URI;
 
 const connection = mongoose
-  .connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(mongoURI)
   .then(() => console.log("DB connection successful!"))
   .catch((error) => console.error("DB not connected:", error));
 
